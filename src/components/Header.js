7fc6918b07ebd4861b007ae4d4c9b82066e8b05d
Nicola Milne <nicolalouisemilne@gmail.com>
2022-06-08 15:43:54 -0600

    commit tree 374b3153424796b85e4d4e93f1faa312ae2bb07e

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-function Header({ isDarkMode, onDarkModeClick }) {
+function Header({ isDarkMode, handleDarkModeClick }) {
   return (
     <header>
       <h2>Shopster</h2>
-      <button onClick={onDarkModeClick}>
+      <button onClick={handleDarkModeClick}>
         {isDarkMode ? "Dark" : "Light"} Mode
       </button>
     </header>
--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({onItemFormSubmit}) {
+
+  const [formData, setFormData] = useState({
+    name: "",
+    category: "Produce",
+  })
+
+  function handleChange(e){
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setFormData({
+      ...formData,
+      [name] : value,
+    })
+  }
+
+  function handleSubmit(e){
+    e.preventDefault();
+    onItemFormSubmit({
+      id: uuid(),
+      ...formData
+    });
+  }
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="name" value={formData.name} onChange={handleChange}/>
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select name="category" value={formData.category} onChange={handleChange}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
